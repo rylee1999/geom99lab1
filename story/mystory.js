a9f17c0772b98dc21e99942b34a7bc889b76276b
rylee1999 <146375958+rylee1999@users.noreply.github.com>
2024-01-25 21:03:35 -0500

    Update mystory.js

--- story/mystory.js
+++ story/mystory.js
@@ -18,15 +18,7 @@ async function initMap() {
     position: { lat: 37.419, lng: -122.03 },
     title: "Title text for the marker at lat: 37.419, lng: -122.03",
   });
-  // Adjust the scale.
-  const pinScaled = new PinElement({
-    scale: 1.5,
-  });
-  const markerViewScaled = new AdvancedMarkerElement({
-    map,
-    position: { lat: 37.419, lng: -122.02 },
-    content: pinScaled.element,
-  });
+ 
   // Change the background color.
   const pinBackground = new PinElement({
     background: "#FBBC04",
