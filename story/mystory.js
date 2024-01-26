d25685b3b5b40fc12b28367a65e22380b1dfd9f3
rylee1999 <146375958+rylee1999@users.noreply.github.com>
2024-01-26 02:42:17 -0500

    Update mystory.js

--- story/mystory.js
+++ story/mystory.js
@@ -3,7 +3,7 @@
 // Australia which was made by Charles Kingsford Smith.
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 3.5,
     center: { lat:52.299, lng: -0.943},
     mapTypeId: "terrain",
   });
